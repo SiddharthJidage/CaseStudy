417331fec5af6f9e584e373d6f98267d3d62d81a
siddharth jidage <67370709+SiddharthJidage@users.noreply.github.com>
2022-01-03 08:46:29 +0530

    Delete Coupons directory

--- Coupons/src/main/java/com/example/Coupons/CouponRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Coupons;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface  CouponRepository extends MongoRepository<Coupon, String> {
-
-
-}
